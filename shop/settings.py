a7e79109a21fa97518c6211bd23c3ccd1997aedd
Oleg-s-s <salitrunskyy@gmail.com>
2022-06-26 16:25:56 +0300

    update 5.6

--- shop/settings.py
+++ shop/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-zzr^0$y=lk8#h-)*7)1b=%m5!w5!y=q06su%7%wo7f5ks@n_ts
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'radiant-everglades-77387.herokuapp.com']
 
 
 # Application definition
