2183566b279f77988c17717f31dd5c6340bf0ebb
Oleg-s-s <salitrunskyy@gmail.com>
2022-06-26 15:56:26 +0300

    update 5.2

--- shop/settings.py
+++ shop/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-zzr^0$y=lk8#h-)*7)1b=%m5!w5!y=q06su%7%wo7f5ks@n_ts
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['gentle-harbor-34840.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
