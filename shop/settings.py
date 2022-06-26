effdb65a1419e5fba580df689d40c9c7ed6d473a
Oleg-s-s <salitrunskyy@gmail.com>
2022-06-26 16:39:30 +0300

    6.1

--- shop/settings.py
+++ shop/settings.py
@@ -125,7 +125,7 @@ STATIC_URL = 'static/'
 
 STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'files')
 
 MEDIA_URL = "/media/"
 
